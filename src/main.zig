5781d64d624ff26abf838286005e8c83b1246029
soheil <soheil.rasekh.01@gmail.com>
2024-07-31 16:36:14 +0330

    feat: parse DOS Header

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,25 @@
 const std = @import("std");
-const zpe = @import("zpe");
+const PEParser = @import("zpe").PEParser;
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
     var args = std.process.args();
     _ = args.next();
 
-    const file_name = args.next() orelse "";
+    const file_path = args.next() orelse return error.MissingFilePath;
 
-    std.debug.print("file name: {s}\n", .{file_name});
+    var parser = try PEParser.init(allocator, file_path);
+    defer parser.deinit();
 
-    const file = try std.fs.cwd().openFile(file_name, .{});
-    defer file.close();
+    try parser.parse();
+
+    var bw = std.io.bufferedWriter(std.io.getStdOut().writer());
+    const stdout = bw.writer();
+
+    try parser.print(stdout);
+
+    try bw.flush();
 }
--- src/zpe.zig
+++ src/zpe.zig
@@ -1,2 +1,60 @@
 const std = @import("std");
 const win32 = @import("zigwin32").everything;
+
+const assert = std.debug.assert;
+
+pub const PEParser = struct {
+    const Self = @This();
+
+    allocator: std.mem.Allocator,
+    file: std.fs.File,
+    dos_header: ?win32.IMAGE_DOS_HEADER = null,
+
+    // TODO: file path or file content
+    pub fn init(allocator: std.mem.Allocator, file_path: []const u8) !Self {
+
+        // TODO: keeping the file open until the parser is deinitialized or creating a copy of the file content
+        const file = try std.fs.cwd().openFile(file_path, .{});
+
+        return .{
+            .allocator = allocator,
+            .file = file,
+        };
+    }
+
+    pub fn deinit(self: Self) void {
+        self.file.close();
+    }
+
+    pub fn parse(self: *Self) !void {
+        try self.parseDOSHeader();
+    }
+
+    fn parseDOSHeader(self: *Self) !void {
+        // TODO: is it required to seek to 0?
+        try self.file.seekTo(0);
+
+        // TODO: keep the file.reader() as the struct member
+        const reader = self.file.reader();
+
+        const dos_header = try reader.readStruct(win32.IMAGE_DOS_HEADER);
+        if (dos_header.e_magic != win32.IMAGE_DOS_SIGNATURE) return error.InvalidDOSHeaderMagic;
+
+        self.dos_header = dos_header;
+    }
+
+    pub fn print(self: Self, writer: anytype) !void {
+        try self.printDOSHeaderInfo(writer);
+    }
+
+    fn printDOSHeaderInfo(self: Self, writer: anytype) !void {
+        assert(self.dos_header != null);
+        const dos_header = self.dos_header.?;
+
+        try writer.writeAll("DOS Header:\n");
+        try writer.writeAll("-----------\n\n");
+
+        try writer.print("Magic: 0x{X}\n", .{dos_header.e_magic});
+        try writer.print("File address of new exe header: 0x{X}\n", .{dos_header.e_lfanew});
+    }
+};
