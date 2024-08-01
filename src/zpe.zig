cab5e35c9696677923d745c0710e34cc12158734
soheil <soheil.rasekh.01@gmail.com>
2024-08-01 16:40:54 +0330

    feat: parse and print Rich Header

--- src/zpe.zig
+++ src/zpe.zig
@@ -1,14 +1,20 @@
 const std = @import("std");
 const win32 = @import("zigwin32").everything;
-
 const assert = std.debug.assert;
 
 pub const PEParser = struct {
     const Self = @This();
 
+    const RICH_HEADER_ENTRY = struct {
+        build_id: u16,
+        product_id: u16,
+        use_count: u32,
+    };
+
     allocator: std.mem.Allocator,
     file: std.fs.File,
     dos_header: ?win32.IMAGE_DOS_HEADER = null,
+    rich_header_entries: ?[]RICH_HEADER_ENTRY = null,
 
     // TODO: file path or file content
     pub fn init(allocator: std.mem.Allocator, file_path: []const u8) !Self {
@@ -24,10 +30,12 @@ pub const PEParser = struct {
 
     pub fn deinit(self: Self) void {
         self.file.close();
+        if (self.rich_header_entries) |rich_header_entries| self.allocator.free(rich_header_entries);
     }
 
     pub fn parse(self: *Self) !void {
         try self.parseDOSHeader();
+        try self.parseRichHeader();
     }
 
     fn parseDOSHeader(self: *Self) !void {
@@ -43,8 +51,63 @@ pub const PEParser = struct {
         self.dos_header = dos_header;
     }
 
+    fn parseRichHeader(self: *Self) !void {
+        assert(self.dos_header != null);
+
+        const dos_header = self.dos_header.?;
+
+        try self.file.seekTo(0);
+
+        const reader = self.file.reader();
+
+        var buffer = try self.allocator.alloc(u8, @intCast(dos_header.e_lfanew));
+        defer self.allocator.free(buffer);
+
+        try reader.readNoEof(buffer);
+
+        const rich_id_index = std.mem.indexOf(u8, buffer, "Rich") orelse return;
+
+        const checksum_text = buffer[rich_id_index + 4 .. rich_id_index + 8];
+        const checksum_mask = "DanS";
+
+        var start_marker: [4]u8 = undefined;
+        for (0..4) |byte_index| start_marker[byte_index] = checksum_text[byte_index] ^ checksum_mask[byte_index];
+
+        const rich_header_start_index = std.mem.indexOf(u8, buffer, &start_marker) orelse return;
+
+        const rich_header_payload = buffer[rich_header_start_index + 16 .. rich_id_index];
+
+        // TODO: is it required to check the entries length?
+        assert(rich_header_payload.len % 8 == 0);
+
+        var rich_header_entries = try self.allocator.alloc(RICH_HEADER_ENTRY, rich_header_payload.len / 8);
+
+        var entry_offset: usize = 0;
+        while (entry_offset < rich_header_payload.len) : (entry_offset += 8) {
+            var encrypted_comp_id = rich_header_payload[entry_offset .. entry_offset + 4];
+            for (0..4) |byte_index| encrypted_comp_id[byte_index] = encrypted_comp_id[byte_index] ^ checksum_text[byte_index];
+
+            var encrypted_count = rich_header_payload[entry_offset + 4 .. entry_offset + 8];
+            for (0..4) |byte_index| encrypted_count[byte_index] = encrypted_count[byte_index] ^ checksum_text[byte_index];
+
+            const build_id = std.mem.readInt(u16, encrypted_comp_id[0..2], .little);
+            const product_id = std.mem.readInt(u16, encrypted_comp_id[2..4], .little);
+            const use_count = std.mem.readInt(u32, encrypted_count[0..4], .little);
+
+            rich_header_entries[entry_offset / 8] = .{
+                .build_id = build_id,
+                .product_id = product_id,
+                .use_count = use_count,
+            };
+        }
+
+        self.rich_header_entries = rich_header_entries;
+    }
+
     pub fn print(self: Self, writer: anytype) !void {
         try self.printDOSHeaderInfo(writer);
+        try writer.writeAll("\n");
+        try self.printRichHeaderInfo(writer);
     }
 
     fn printDOSHeaderInfo(self: Self, writer: anytype) !void {
@@ -91,5 +154,16 @@ pub const PEParser = struct {
         try writer.writeAll("\n");
 
         try writer.print("  File address of new exe header: 0x{X}\n", .{dos_header.e_lfanew});
+    }
+
+    fn printRichHeaderInfo(self: Self, writer: anytype) !void {
+        const rich_header_entries = self.rich_header_entries orelse return;
+
+        try writer.writeAll("Rich Header:\n");
+        try writer.writeAll("------------\n\n");
+
+        for (rich_header_entries) |entry| {
+            try writer.print(" 0x{X:0>4} 0x{X:0>4} 0x{X:0>8}: {d}.{d}.{d}\n", .{ entry.build_id, entry.product_id, entry.use_count, entry.build_id, entry.product_id, entry.use_count });
+        }
     }
 };
